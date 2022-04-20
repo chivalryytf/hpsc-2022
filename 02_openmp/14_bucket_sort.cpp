2e39aa79373bf5bb78c079d67fe063e5a4555c2e
arsen <chivalryytf@gmail.com>
2022-04-21 03:16:46 +0900

    HW01

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -18,13 +18,20 @@ int main() {
   std::vector<int> offset(range,0);
   for (int i=1; i<range; i++) 
     offset[i] = offset[i-1] + bucket[i-1];
-  #pragma omp parallel for
-  for (int i=0; i<range; i++) {
-    int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
+  // for (int i=0; i<range; i++) {
+  //   int j = offset[i];
+  //   for (; bucket[i]>0; bucket[i]--) {
+  //     key[j++] = i;
+  //   }
+  // }
+  #pragma omp parallel
+  for (int i=0; i<range; i++){
+    #pragma omp for
+    for (int j=offset[i]; j<offset[i]+bucket[i];j++) {
+      key[j] = i;
     }
   }
+
 
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
